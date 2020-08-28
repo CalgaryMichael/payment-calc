39f675fe3998f3472fd1998baed2b0aa1bee2057
Calgary Michael <cseth.michael@gmail.com>
2020-08-27 22:12:56 -0400

    convert to argparse

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 
@@ -5,8 +6,17 @@ from payment_calc.models import Scenario
 from payment_calc.payment_calc import decrease_debt
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('scenario_file', type=str)
+    parser.add_argument('--sort', type=str, default='debt_total')
+    parser.add_argument('--sort-reverse', action='store_true', default=False)
+    args = parser.parse_args()
+
+    scenario = Scenario.of(args.scenario_file)
+    decrease_debt(scenario, sort_key=args.sort, reverse=args.sort_reverse)
+
+
 if __name__ == '__main__':
-    file_loc = os.path.abspath(sys.argv[1])
-    scenario = Scenario.of(file_loc)
-    decrease_debt(scenario, sort_key='payments', reverse=True)
+    main()
 
