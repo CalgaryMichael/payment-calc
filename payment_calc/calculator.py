3048d7145398834ebdf7682396f04004283b55f7
Calgary Michael <cseth.michael@gmail.com>
2020-09-03 22:11:09 -0400

    refresh debts after every month -- no more 'payment_clocks' bullshit

--- payment_calc/calculator.py
+++ payment_calc/calculator.py
@@ -25,14 +25,14 @@ def build_projected_debt_reduction(
     save.write_outcomes_to_file(output_fp, outcomes)
 
 
-def reduce_debts(debts: List[Debt], start_date: datetime.date) -> Iterator[Outcome]:
-    current_date = start_date
+def reduce_debts(debts: List[Debt], current_date: datetime.date) -> Iterator[Outcome]:
     while True:
-        outcome = reduce_debts_for_month(debts, start_date, current_date)
+        current_date = date_utils.next_month(current_date)
+        outcome = reduce_debts_for_month(debts, current_date)
         yield outcome
         if not outcome.outstanding_debt():
             break
-        current_date = date_utils.next_month(current_date)
+        debts = refresh_debts(debts, outcome)
 
 
 def reduce_debts_for_month(
@@ -41,14 +41,12 @@ def reduce_debts_for_month(
         end_date: datetime.date
 ) -> Outcome:
     """Calculate debt reduction cycle for the amount of months between the provided dates"""
-    payment_clocks = date_utils.month_diff(end_date, start_date)
     debt_outcomes = []
     remainder = 0
     for i, debt in enumerate(debts):
         debt_outcome, remainder = reduce_debt(
             debt,
             end_date,
-            payment_clocks,
             remainder
         )
         debt_outcomes.append(debt_outcome)
@@ -61,16 +59,34 @@ def reduce_debts_for_month(
 def reduce_debt(
         debt: Debt,
         current_date: datetime.date,
-        payment_clocks: int,
         remainder: float
 ) -> Tuple[DebtOutcome, float]:
-    payment_total = (sum_active_payments(debt, current_date) * payment_clocks) + remainder
-    debt_total, remainder = subtract_with_remainder(debt.debt_total, payment_total, debt.interest_rate)
+    payment_total = sum_active_payments(debt, current_date) + remainder
+    debt_total, remainder = subtract_with_remainder(
+        debt.debt_total,
+        payment_total,
+        debt.interest_rate
+    )
     debt_outcome = DebtOutcome(
         debt_name=debt.debt_name,
         debt_total=debt_total
     )
     return debt_outcome, remainder
+
+
+def refresh_debts(debts: List[Debt], last_outcome: DebtOutcome) -> List[Debt]:
+    """
+    Return a list of debts with the debt totals from the last outcome.
+    List will be ordered with the settled debts at the top.
+    """
+    def _map_debt(debt: Debt) -> Debt:
+        for debt_outcome in last_outcome.debt_outcomes:
+            if debt.debt_name == debt_outcome.debt_name:
+                debt.debt_total = debt_outcome.debt_total
+        return debt
+
+    output = list(map(_map_debt, debts[:]))
+    return output
 
 
 def sum_active_payments(debt: Debt, current_date: datetime.date) -> float:
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import datetime
 from payment_calc import calculator as under_test
-from payment_calc.models.outcome import DebtOutcome
+from payment_calc.models.outcome import DebtOutcome, Outcome
 from payment_calc.models.scenario import Debt, DebtPayment
 
 
@@ -8,7 +8,6 @@ def test_reduce_debt__no_remainder(debt: Debt):
     actual_debt_outcome, actual_remainder = under_test.reduce_debt(
         debt=debt,
         current_date=datetime.date(2020, 3, 1),
-        payment_clocks=1,
         remainder=0.00
     )
     expected_debt_outcome = DebtOutcome(debt_name='jazz', debt_total=91.00)
@@ -21,13 +20,110 @@ def test_reduce_debt__with_remainder(debt: Debt):
     actual_debt_outcome, actual_remainder = under_test.reduce_debt(
         debt=debt,
         current_date=datetime.date(2020, 3, 1),
-        payment_clocks=1,
         remainder=120.00
     )
     expected_debt_outcome = DebtOutcome(debt_name='jazz', debt_total=0.00)
     expected_remainder = 29.00
     assert actual_debt_outcome == expected_debt_outcome
     assert actual_remainder == expected_remainder
+
+
+def test_refresh_debts__no_change():
+    debts = [
+        Debt(
+            debt_name='jazz',
+            debt_total=100.00,
+            payments=[DebtPayment(amount=10.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        )
+    ]
+    outcome = Outcome(
+        effective_date=None,
+        debt_outcomes=[
+            DebtOutcome(
+                debt_name='jazz',
+                debt_total=50.00
+            )
+        ]
+    )
+    actual = under_test.refresh_debts(debts, outcome)
+    assert actual == debts
+
+
+def test_refresh_debts__debt_reduced():
+    debts = [
+        Debt(
+            debt_name='jazz',
+            debt_total=100.00,
+            payments=[DebtPayment(amount=10.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        )
+    ]
+    outcome = Outcome(
+        effective_date=None,
+        debt_outcomes=[
+            DebtOutcome(
+                debt_name='jazz',
+                debt_total=50.00
+            )
+        ]
+    )
+    actual = under_test.refresh_debts(debts, outcome)
+    expected = [
+        Debt(
+            debt_name='jazz',
+            debt_total=50.00,
+            payments=[DebtPayment(amount=10.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        )
+    ]
+    assert actual == expected
+
+
+def test_refresh_debts__settled_at_top():
+    debts = [
+        Debt(
+            debt_name='jazz 1',
+            debt_total=100.00,
+            payments=[DebtPayment(amount=10.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        ),
+        Debt(
+            debt_name='jazz 2',
+            debt_total=200.00,
+            payments=[DebtPayment(amount=25.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        )
+    ]
+    outcome = Outcome(
+        effective_date=None,
+        debt_outcomes=[
+            DebtOutcome(
+                debt_name='jazz 1',
+                debt_total=50.00
+            ),
+            DebtOutcome(
+                debt_name='jazz 2',
+                debt_total=0.00
+            )
+        ]
+    )
+    actual = under_test.refresh_debts(debts, outcome)
+    expected = [
+        Debt(
+            debt_name='jazz 2',
+            debt_total=0.00,
+            payments=[DebtPayment(amount=25.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        ),
+        Debt(
+            debt_name='jazz 1',
+            debt_total=50.00,
+            payments=[DebtPayment(amount=10.00, start_date='2020-01', end_date='2020-06')],
+            interest_rate=0.12
+        )
+    ]
+    assert actual == expected
 
 
 def test_sum_active_payments__no_payments():
